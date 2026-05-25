3023fc0748daca82500a769a2d0cd7aa8116df63
bettyhubcod <ndiayebetty333@gmail.com>
2026-05-25 20:06:39 +0000

    ajout de la structure jsx du navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,38 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 function Navbar() {
-  return <nav>Navbar</nav>
+   const [etat,setState]=useState(false)
+
+   
+
+   
+
+  return (
+
+    <nav>
+
+      <div>  <Link to="/">photo</Link></div>
+   
+      <button onClick={()=>setState(prev=>!prev)}>{etat?<X/>:<Menu/>}</button>
+      {etat &&<ul >
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/galerie">Galerie</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>}
+    
+    </nav>
+
+  );
 }
 
-export default Navbar 
+export default Navbar;
