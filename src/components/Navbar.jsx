83eac74488161555d11e307d81317f4c833a4741
bettyhubcod <ndiayebetty333@gmail.com>
2026-05-31 18:14:38 +0000

    feat: navbar responsive desktop + mobile complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import "../styles/Navbar.css";
-import logo from "../assets/logo.png"
+import logo from "../assets/logo.png";
 
 function Navbar() {
   const [etat, setState] = useState(false);
@@ -10,9 +10,28 @@ function Navbar() {
     <nav>
       <div className="menu">
         <div>
-          <NavLink to="/" >
+          <NavLink to="/">
             <img src={logo} alt="Le Suprême" />
           </NavLink>
+        </div>
+
+        <div className="nav-desktop">
+          <ul>
+            <li>
+              <NavLink to="/">Accueil</NavLink>
+            </li>
+            <li>
+              <NavLink to="/menu">Menu</NavLink>
+            </li>
+            <li>
+              <NavLink to="/galerie">Galerie</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" id="contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
         </div>
 
         <button onClick={() => setState((prev) => !prev)}>
@@ -21,22 +40,27 @@ function Navbar() {
       </div>
       <div className="ligne_decor"></div>
 
-      {etat && (
-        <ul>
-          <li>
-            <NavLink to="/">Accueil</NavLink>
-          </li>
-          <li>
-            <NavLink to="/menu">Menu</NavLink>
-          </li>
-          <li>
-            <NavLink to="/galerie">Galerie</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact" id="contact">Contact</NavLink>
-          </li>
-        </ul>
-      )}
+      <div className="nav-mobile">
+        {etat && (
+          <ul>
+            <li>
+              <NavLink to="/">Accueil</NavLink>
+            </li>
+            <li>
+              <NavLink to="/menu">Menu</NavLink>
+            </li>
+            <li>
+              <NavLink to="/galerie">Galerie</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" id="contact">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        )}
+      </div>
+
       <div className="ligne_decor"></div>
     </nav>
   );
